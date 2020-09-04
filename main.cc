6b0750c435f6857a56707adb9120abbfbacb7ac6
why <136046355@qq.com>
2020-09-04 18:02:36 +0800

    28

--- main.cc
+++ main.cc
@@ -1,270 +1,96 @@
 #include <iostream>
 #include <vector>
-#include <stack>
-#include <map>
 #include <unordered_map>
+#include <map>
 
 using namespace std;
 
 // 通用数据结构
-struct ListNode {
+struct TreeNode {
     int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-
-// 链表类练习
-namespace listDemo{
-    // 从尾到头打印链表——栈
-    vector<int> reversePrint(ListNode* head) {
-        if(head == NULL){
-            return {};
-        }
-        vector<int> ret;
-        stack<ListNode*> s;
-        ListNode* cur = head;
-        while(cur != NULL){
-            s.push(cur);
-            cur = cur->next;
-        }
-        while(!s.empty()){
-            ret.push_back(s.top()->val);
-            s.pop();
-        }
-        return ret;
-    }
-
-    // 从尾到头打印链表——递归
-    vector<int> ret;
-    vector<int> reversePrint2(ListNode* head){
-        if(head == NULL){
-            return {};
-        }
-        reversePrint2(head->next);
-        ret.push_back(head->val);
-        return ret;
-    }
-
-    // 删除链表中指定的节点
-    ListNode* deleteNode(ListNode* head, int val) {
-        if(head == NULL){
+namespace TreeDemo{
+    TreeNode* buildTree(int preRoot, int inorderStart, int inorderEnd, unordered_map<int, int>& indexMap, vector<int>& preorder, vector<int>& inorder){
+        if(inorderStart > inorderEnd){
             return NULL;
         }
-        if(head->val == val){
-            return head->next;
-        }
-        ListNode* cur = head;
-        ListNode* pre = head;
-        while(cur != NULL){
-            if(cur->val == val){
-                pre->next = cur->next;
-                break;
-            }
-            pre = cur;
-            cur = cur->next;
-        }
-        return head;
+        // 根节点
+        TreeNode* root = new TreeNode(preorder[preRoot]);
+        // 找到根节点在中序的位置
+        int index = indexMap[preorder[preRoot]];
+        // 左孩子
+        root->left = buildTree(preRoot + 1, inorderStart, index - 1, indexMap, preorder, inorder);
+        // 右孩子, 先序位置为当前位置加上中序中左孩子个数+1
+        root->right = buildTree(preRoot + index - inorderStart + 1, index + 1, inorderEnd, indexMap, preorder, inorder);
+        return root;
     }
 
-    // 删除链表中指定的节点——递归
-    ListNode* deleteNode2(ListNode* head, int val){
-        if(head == NULL){
-            return head;
-        }
-        if(head->val == val){
-            return head->next;
-        }
-        head->next = deleteNode2(head->next, val);
-        return head;
-    }
-
-    // 链表中倒数第K个节点
-    ListNode* getKthFromEnd(ListNode* head, int k) {
-        if(head == NULL){
+    // 输入前序和中序遍历结果，重建二叉树
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if(preorder.size() == 0){
             return NULL;
         }
-        ListNode* fast = head;
-        ListNode* slow = head;
-        while(k--){
-            fast = fast->next;
+        // 将中序遍历的值与索引存入map，方便查找
+        unordered_map<int, int> indexMap;
+        for(int i = 0; i < inorder.size(); i++){
+            indexMap.insert(make_pair(inorder[i], i));
         }
-        while(fast){
-            fast = fast->next;
-            slow = slow->next;
-        }
-        return slow;
+        return buildTree(0, 0, inorder.size() - 1, indexMap, preorder, inorder);
     }
 
-    // 链表中倒数第K个节点——递归
-    int size = 0;
-    ListNode* getKthFromEnd2(ListNode* head, int k){
-        if(head == NULL){
-            return head;
+    bool helper(TreeNode* A, TreeNode* B){
+        if(B == NULL){
+            return true;
         }
-        ListNode* node = getKthFromEnd2(head->next, k);
-        if(++size == k){
-            return head;
+        if(A == NULL || A->val != B->val){
+            return false;
         }
-        return node;
+        return helper(A->left, B->left) && helper(A->right, B->right);
     }
 
-    // 反转链表
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL){
+    // 树的子结构
+    bool isSubStructure(TreeNode* A, TreeNode* B) {
+        if(A == NULL || B == NULL){
+            return false;
+        }
+        if(A->val == B->val && helper(A, B)){
+            return true;
+        }
+        return isSubStructure(A->left, B) || isSubStructure(A->right, B);
+    }
+
+    // 二叉树的镜像
+    TreeNode* mirrorTree(TreeNode* root) {
+        if(root == NULL){
             return NULL;
         }
-        ListNode* cur = head;
-        ListNode* pre = NULL;
-        while(cur){
-            ListNode* next = cur->next;
-            cur->next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
+        TreeNode* tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        mirrorTree(root->left);
+        mirrorTree(root->right);
+        return root;
     }
 
-    // 反转链表——递归
-    ListNode* reverseList2(ListNode* head){
-        if(head == NULL || head->next == NULL){
-            return head;
+    bool recur(TreeNode* left, TreeNode* right){
+        if(left == NULL && right == NULL){
+            return true;
         }
-        ListNode* ret = reverseList2(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return ret;
+        if(left == NULL || right == NULL || left->val != right->val){
+            return false;
+        }
+        return recur(left->left, right->right) && recur(left->right, right->left);
     }
 
-    // 合并两个有序链表
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        // 利用伪节点
-        ListNode* dum = new ListNode(0);
-        ListNode* cur = dum;
-        while(l1 && l2){
-            if(l1->val > l2->val){
-                cur->next = l2;
-                l2 = l2->next;
-            }else{
-                cur->next = l1;
-                l1 = l1->next;
-            }
-            cur = cur->next;
+    // 对称二叉树
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL){
+            return true;
         }
-        cur->next = l1 == NULL ? l2 : l1;
-        ListNode* ret = dum->next;
-        delete dum;
-        return ret;
-    }
-
-    //复杂链表的复制
-    Node* copyRandomList(Node* head) {
-        if(head == NULL){
-            return NULL;
-        }
-        unordered_map<Node*, Node*> um;
-        Node* cur = head;
-        while(cur){
-            um.insert(make_pair(cur, new Node(cur->val)));
-            cur = cur->next;
-        }
-        cur = head;
-        while(cur){
-            Node* node = um.find(cur)->second;
-            if(cur->next){
-                node->next = um.find(cur->next)->second;
-            }
-            if(cur->random){
-                node->random = um.find(cur->random)->second;
-            }
-            cur = cur->next;
-        }
-        return um.find(head)->second;
-    }
-
-    namespace test1{
-        class Node {
-            public:
-                int val;
-                Node* left;
-                Node* right;
-
-                Node() {}
-
-                Node(int _val) {
-                    val = _val;
-                    left = NULL;
-                    right = NULL;
-                }
-
-                Node(int _val, Node* _left, Node* _right) {
-                    val = _val;
-                    left = _left;
-                    right = _right;
-                }
-        };
-        
-        Node* head = NULL;
-        Node* pre = NULL;
-
-        void bfs(Node* cur){
-            if(cur == NULL){
-                return;
-            }
-            bfs(cur->left);
-            if(pre == NULL){
-                head = cur;
-            }else{
-                pre->right = cur;
-            }
-            cur->left = pre;
-            pre = cur;
-            bfs(cur->right);
-        }
-
-        // 二叉搜索树转双向循环链表，左孩子指向前驱，右孩子指向后继
-        Node* treeToDoublyList(Node* root) {
-            if(root == NULL){
-                return NULL;
-            }
-            bfs(root);
-            head->left = pre;
-            pre->right = head;
-            return head;
-        }
-    }
-
-    // 两个链表的第一个公共节点
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if(headA == NULL || headB == NULL){
-            return NULL;
-        }
-        ListNode* curA = headA;
-        ListNode* curB = headB;
-        while(curA != curB){
-            if(curA != NULL){
-                curA = curA->next;
-            }else{
-                curA = headB;
-            }
-            if(curB != NULL){
-                curB = curB->next;
-            }else{
-                curB = headA;
-            }
-        }
-        return curA;
+        return recur(root->left, root->right);
     }
 }
